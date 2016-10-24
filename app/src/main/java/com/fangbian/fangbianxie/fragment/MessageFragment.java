9e6edba38009c8f330d2e3f1f7796099691c51b5
sunxipeng <1141351242@qq.com>
2016-10-24 13:35:44 +0800

    添加下拉刷新，上拉加载！

--- app/src/main/java/com/fangbian/fangbianxie/fragment/MessageFragment.java
+++ app/src/main/java/com/fangbian/fangbianxie/fragment/MessageFragment.java
@@ -1,10 +1,10 @@
 package com.fangbian.fangbianxie.fragment;
 
 import android.view.View;
-import android.widget.ListView;
 
 import com.fangbian.fangbianxie.R;
 import com.fangbian.fangbianxie.adapter.MessageAdapter;
+import com.fangbian.fangbianxie.view.XListView;
 
 /**
  * Created by sunxipeng on 2016/10/12.
@@ -18,7 +18,9 @@ public class MessageFragment extends BaseFragment {
     @Override
     protected void initView(View view) {
 
-        ListView listview = (ListView) view.findViewById(R.id.listview);
+        XListView listview = (XListView) view.findViewById(R.id.listview);
+        listview.setPullRefreshEnable(true);
+        listview.setPullLoadEnable(true);
         listview.setAdapter(new MessageAdapter(getActivity()));
 
     }
